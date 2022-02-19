e1f7139715d604f01bd825b9773c802e5bc7acbf
Suh Youri <suhyouri@gmail.com>
2022-02-19 20:57:50 +0900

    gps data server side

--- server.js
+++ server.js
@@ -1,18 +1,60 @@
 const express = require("express");
+// const bodyParser = require("body-parser")
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-io.on("connection", (socket) => {
-  console.log("a user connected");
+let lat;
+let long;
+let gpsdms;
+
+let gpsData = {
+    lat: lat,
+    long: long,
+    gpsdms: gpsdms
+}
+
+//setup basic json
+app.get("/gps", (req, res) => {
+  res.json(gpsData);
+  // return res.json({
+  //   lat: req.body.lat,
+  //   long: req.body.long,
+  // });
 });
 
-server.listen(3000, () => {
-  console.log("listening on *:3000");
+//event value
+app.post("/gps", (req, res) => {
+  console.log(req.body.lat);
+  console.log(req.body.long);
+  console.log(req.body.gpsdms);
+  gpsData.lat = req.body.lat;
+  gpsData.long = req.body.long;
+  gpsData.gpsdms = req.body.gpsdms;
+  res.json(gpsData);
+  // -----> Data to send to html
+  io.emit("dd.lat", gpsData.lat);
+  io.emit("dd.long", gpsData.long);
+  io.emit("finalGPS", gpsData.gpsdms);
 });
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  // console.log(socket);
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(5500, () => {
+  console.log("listening on *:5500");
+});
+
